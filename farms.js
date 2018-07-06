485c7a1ba4dbd72518922664474049c2e02041a7
OscarHavril <40829608+OscarHavril@users.noreply.github.com>
2018-07-06 18:26:15 +0200

    Update farms.js

--- farms.js
+++ farms.js
@@ -9,32 +9,32 @@ sheets:{'cookieSheet':'https://raw.githubusercontent.com/Generatoror/hello-world
 func:function()
 {
 
-  G.resCategories={
-    'animals':{
+ 	 G.resCategories={
+    		'animals':{
 			name:'Animals',
 			base:[],
 			side:[],
 		},
   }
-  new G.Res({
+  	new G.Res({
 		name:'seed',
 		desc:'[seed] is not very tasty or healthy. Truely not',
 		icon:[0,0,'cookieSheet'],
-		turnToByContext:{'eat':{'health':0.02,'happiness':0.005},'decay':{'spoiled food':0.02},
+		turnToByContext:{'eat':{'health':0.02,'happiness':0.005},'decay':{'spoiled food':0.1},
 		partOf:'food',
 		category:'food',
 	});
 	new G.Res({
 		name:'wheat',
-		desc:'[wheat] is not very tasty or healthy. Hmm..I've already seen this somwhere..',
+		desc:'[wheat] is not very tasty or healthy. Hmm..I\'ve already seen this somwhere..',
 		icon:[0,0,'cookieSheet'],
 		turnToByContext:{'eat':{'health':0.02,'happiness':0.01},'decay':{'spoiled food':0.5}},//this basically translates to : "when eaten, generate some health and happiness; when rotting, turn into either nothing or some spoiled food"
 		partOf:'food',
 		category:'food',
 	});
-  new G.Res({
+  	new G.Res({
 		name:'egg',
-		desc:'Well, it's eggs.', //Yeah, edit me that, maybe.
+		desc:'Well, it\'s eggs.', //Yeah, edit me that, maybe.
 		icon:[1,0,'cookieSheet'],
 		turnToByContext:{'eat':{'health':0.005,'happiness':1},'decay':{'egg':0.7,'spoiled food':0.3}},
 		partOf:'food',
@@ -42,33 +42,33 @@ func:function()
 	});
 	new G.Res({
 		name:'chicken',
-		desc:'Cached by [hunter]s, they ', //Yeah, edit me that
+		desc:'Cached by [hunter]s, they..Oh.', //Yeah, edit me that
 		icon:[1,0,'cookieSheet'],
 		turnToByContext:{'decay':{'chichen':0.95,'meat':0.05}},//yes, a chicken can die...
 		partOf:'animals',
 		category:'animals',
 	});
-  G.unitCategories.push(
+  	G.unitCategories.push(
 		{id:'farming',name:'Farming'},
 	);
-  new G.Unit({
+ 	 new G.Unit({
 		name:'farm',
-		desc:'@[farm]s lets you produce specials goods like wheed.<>Yep... Oh wait I had written wheed, sorry, it's [wheat], of course, yes.',
-		icon:[7,2],
+		desc:'@[farm]s lets you produce specials goods like wheed.<>Yep... Oh wait I had written wheed, sorry, it\'s [wheat], of course, yes.',
+		icon:[1,1,'cookieSheet'],
 		cost:{},
 		use:{'worker':1},
 		staff:{'knapped tools':1},
 		upkeep:{'coin':0.1},
-    modes:{
+    		modes:{
 			'plant care':{name:'Farming',icon:[1,1],desc:'Produce wheat',use:{'stone tools':1}},
 			'animal care':{name:'Breeding',icon:[1,1],desc:'Breed animals',use:{'stone tools':1,'chicken':3}},
 		},
 		effects:[
       
-      {type:'mult',value:1.2,req:{'harvest rituals':'on'},mode:'plant care'},
-      {type:'mult',value:1.4,req:{'farming':true},mode:'plant care'}
-      {type:'convert',from:{'water':0.2,'seed':1},into:{'wheat':2},every:1,mode:'plant care'},
-      {type:'convert',from:{'seed':10,},into:{'egg':1},every:5,mode:'animal care'},
+     			{type:'mult',value:1.2,req:{'harvest rituals':'on'},mode:'plant care'},
+      			{type:'mult',value:1.4,req:{'farming':true},mode:'plant care'}
+      			{type:'convert',from:{'water':0.2,'seed':1},into:{'wheat':2},every:1,mode:'plant care'},
+     			{type:'convert',from:{'seed':10,},into:{'egg':1},every:5,mode:'animal care'},
       
 		],
 		req:{'farms':true},
@@ -78,14 +78,14 @@ func:function()
 		//adding seeds as something that can be gathered from grass yep, really cool
 	G.getDict('grass').res['gather']['seed']=6;
 		//adding a new mode to hunter for them being more friendly (I already said it)
-	G.getDict('hunter').modes['cach']={name:'Cach Animals',desc:'The hunter don't kill, they try to be friendly.',req:{'caching':true}};
+	G.getDict('hunter').modes['cach']={name:'Cach Animals',desc:'The hunter don\'t kill, they try to be friendly.',req:{'caching':true}};
 		//adding a new effect to artisans that handles the actual hot sauce preparing and is only active when the unit has the mode "hot sauce"
 	G.getDict('hunter').effects.push({type:'gather',context:'gather',what:{'chicken':0.1},amount:0.1,max:0.1,mode:'caching'});
 	
 	
 	new G.Tech({
 		name:'caching',
-		desc:'@[hunter]s can now cach Animals and put them into farms//You'll of course need these farms from preventing your animals dying from corpses or running out.',
+		desc:'@[hunter]s can now cach Animals and put them into farms//You\'ll of course need these farms from preventing your animals dying from corpses or running out.',
 		icon:[0,1,'cookieSheet'],
 		cost:{'insight':10},
 		req:{'farms':true},
