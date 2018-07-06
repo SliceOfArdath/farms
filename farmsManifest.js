e3b5a36df64582f53a40ab2f2524b2c6c65cdbb9
OscarHavril <40829608+OscarHavril@users.noreply.github.com>
2018-07-06 16:43:52 +0200

    Create farmsManifest.js

---
+++ farmsManifest.js
@@ -0,0 +1,9 @@
+G.DeclareManifest({
+name:'farmsManifest',
+updates:{
+	'Example mod*':'mod.js',//we're updating any mod with a name that begins with "Example mod"
+	'Test mod*':'https://rawgit.com/Generatoror/hello-world/master/cookieLegacy.js',
+	'Thot mod*':'https://rawgit.com/torcado194/neverending-legacy-mods/master/thot.js'
+  'Farms*':'https://rawgit.com/OscarHavril/farms/master/farms.js'
+}
+});
