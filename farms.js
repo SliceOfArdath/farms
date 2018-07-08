bb1dbee05b764236be14725c3a90df46e8c69476
OscarHavril <40829608+OscarHavril@users.noreply.github.com>
2018-07-08 14:42:09 +0200

    Update farms.js

--- farms.js
+++ farms.js
@@ -5,7 +5,7 @@ desc:'A simple mod that add farming. Needed for Futuristic Enginering',
 engineVersion:1,
 manifest:'https://rawgit.com/OscarHavril/farms/master/farmsManifest.js',
 requires:['Default dataset*'],
-sheets:{'cookieSheet':'https://raw.githubusercontent.com/Generatoror/hello-world/master/sprits.png'},//custom stylesheet (note : broken in IE and Edge for the time being) Note2 : to Edit
+sheets:{'farmsSheet':'https://raw.githubusercontent.com/OscarHavril/farms/master/pixil-frame-0(3).png'},//custom stylesheet (note : broken in IE and Edge for the time being)
 func:function()
 {
 
@@ -19,7 +19,7 @@ func:function()
   	new G.Res({
 		name:'seed',
 		desc:'[seed] is not very tasty or healthy. Truely not',
-		icon:[0,0,'cookieSheet'],
+		icon:[0,0,'farmsSheet'],
 		turnToByContext:{'eat':{'health':0.02,'happiness':0.005},'decay':{'spoiled food':0.1},
 		partOf:'food',
 		category:'food',
@@ -27,7 +27,7 @@ func:function()
 	new G.Res({
 		name:'wheat',
 		desc:'[wheat] is not very tasty or healthy. Hmm..I\'ve already seen this somwhere..',
-		icon:[0,0,'cookieSheet'],
+		icon:[0,2,'farmsSheet'],
 		turnToByContext:{'eat':{'health':0.02,'happiness':0.01},'decay':{'spoiled food':0.5}},//this basically translates to : "when eaten, generate some health and happiness; when rotting, turn into either nothing or some spoiled food"
 		partOf:'food',
 		category:'food',
@@ -35,15 +35,15 @@ func:function()
   	new G.Res({
 		name:'egg',
 		desc:'Well, it\'s eggs.', //Yeah, edit me that, maybe.
-		icon:[1,0,'cookieSheet'],
+		icon:[0,3,'farmsSheet'],
 		turnToByContext:{'eat':{'health':0.005,'happiness':1},'decay':{'egg':0.7,'spoiled food':0.3}},
 		partOf:'food',
 		category:'food',
 	});
 	new G.Res({
 		name:'chicken',
-		desc:'Cached by [hunter]s, they..Oh.', //Yeah, edit me that
-		icon:[1,0,'cookieSheet'],
+		desc:'Cached by [hunter]s, they will produce eggs if there are farms', 
+		icon:[0,1,'farmsSheet'],
 		turnToByContext:{'decay':{'chichen':0.95,'meat':0.05}},//yes, a chicken can die...
 		partOf:'animals',
 		category:'animals',
@@ -54,14 +54,13 @@ func:function()
  	 new G.Unit({
 		name:'farm',
 		desc:'@[farm]s lets you produce specials goods like wheed.<>Yep... Oh wait I had written wheed, sorry, it\'s [wheat], of course, yes.',
-		icon:[1,1,'cookieSheet'],
-		cost:{},
+		icon:[1,0,'farmsSheet'],
+		cost:{'basic building materials':300},
 		use:{'worker':1},
 		staff:{'knapped tools':1},
-		upkeep:{'coin':0.1},
     		modes:{
-			'plant care':{name:'Farming',icon:[1,1],desc:'Produce wheat',use:{'stone tools':1}},
-			'animal care':{name:'Breeding',icon:[1,1],desc:'Breed animals',use:{'stone tools':1,'chicken':3}},
+			'plant care':{name:'Farming',icon:[0,0,'farmsSheet'],desc:'Produce wheat',use:{'stone tools':1}},
+			'animal care':{name:'Breeding',icon:[0,1,'farmsSheet'],desc:'Breed animals',use:{'stone tools':1,'chicken':3}},
 		},
 		effects:[
       
@@ -86,14 +85,14 @@ func:function()
 	new G.Tech({
 		name:'caching',
 		desc:'@[hunter]s can now cach Animals and put them into farms//You\'ll of course need these farms from preventing your animals dying from corpses or running out.',
-		icon:[0,1,'cookieSheet'],
+		icon:[0,1,'farmsSheet'],
 		cost:{'insight':10},
 		req:{'farms':true},
 	});
         new G.Tech({
 		name:'farms',
 		desc:'@You can now build farms who let you produce plants//(And store Animals, yep.)',
-		icon:[0,1,'cookieSheet'],
+		icon:[1,0,'farmsSheet'],
 		cost:{'insight':20},
 		req:{'building':true},
 	});
@@ -101,7 +100,7 @@ func:function()
 	new G.Trait({
 		name:'Farmers',
 		desc:'@[farm]s are now truely more efficient',
-		icon:[1,1,'cookieSheet'],
+		icon:[1,1,'farmsSheet'],
 		chance:5,
 		req:{'farms':true},
 	});
