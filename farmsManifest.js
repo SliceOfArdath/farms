89c3afdecf74ceb2b0f951da37714402c9c76a9a
OscarHavril <40829608+OscarHavril@users.noreply.github.com>
2018-08-20 18:24:06 +0200

    Update farmsManifest.js

--- farmsManifest.js
+++ farmsManifest.js
@@ -1,7 +1,8 @@
 G.DeclareManifest({
 name:'farms mod Manifest',
 updates:{
-  	'Farms*':'https://rawgit.com/OscarHavril/farms/master/[FE]farms.js',
-  //Hmmm it seems to be not working...
+  	'Farms*':'https://rawgit.com/OscarHavril/farms/master/[FE]farms.js', //Broken
+  //Hmmm it seems to be not working... 
+    
 }
 });
