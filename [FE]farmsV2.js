ab09c3336d0554f5824976bae248cf65bf8bb14b
OscarHavril <40829608+OscarHavril@users.noreply.github.com>
2018-08-20 19:05:23 +0200

    Update [FE]farmsV2.js

--- [FE]farmsV2.js
+++ [FE]farmsV2.js
@@ -41,14 +41,6 @@ func:function()
 		category:'food',
 });
 		new G.Res({
-		name:'seed',
-		desc:'[seed] is not very tasty or healthy. Truely not',
-		icon:[0,1,'imageSheet'],
-		turnToByContext:{'eat':{'health':0.02,'happiness':0.005},'decay':{'spoiled food':0.1}},//this , ges; when rotting, turn ng or some spoiled food"
-		partOf:'food',
-		category:'food',
-	});
-		new G.Res({
 		name:'Berries',
 		desc:'[Berries] taste sweet, but spoil quickly.',
 		icon:[1,1,'imageSheet'],
