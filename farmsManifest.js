40f2cc74c89c7c1b908ed867faf2028bccf2ee39
OscarHavril <40829608+OscarHavril@users.noreply.github.com>
2018-08-20 18:25:48 +0200

    Update farmsManifest.js

--- farmsManifest.js
+++ farmsManifest.js
@@ -3,6 +3,6 @@ name:'farms mod Manifest',
 updates:{
   	'Farms*':'https://rawgit.com/OscarHavril/farms/master/[FE]farms.js', //Broken
   //Hmmm it seems to be not working... 
-    
+    'FarmsV2*':'https://rawgit.com/OscarHavril/farms/master/[FE]farmsV2.js',
 }
 });
