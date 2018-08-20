a94ca4a9463ce9a68ae69626c9684dc8d664b3a7
OscarHavril <40829608+OscarHavril@users.noreply.github.com>
2018-08-20 18:38:53 +0200

    Update [FE]farmsV2.js

--- [FE]farmsV2.js
+++ [FE]farmsV2.js
@@ -10,7 +10,7 @@ desc:'A simple mod that add farming. Needed for Futuristic Enginerin',
 engineVersion:1,
 manifest:'ModManifest.js',
 requires:['Default dataset*'],
-sheets:{'farmsSheet':'https://raw.githubusercontent.com/OscarHavril/farms/master/pixil-frame-0(3).png'},//custom stylesheet (note : broken in IE and Edge for the time being)
+sheets:{'imageSheet':'https://raw.githubusercontent.com/OscarHavril/farms/master/pixil-frame-0(3).png'},//custom stylesheet (note : broken in IE and Edge for the time being)
 func:function()
 	{	
 	
@@ -18,7 +18,7 @@ func:function()
 	new G.Res({
 		name:'Fruit Juice',
 		desc:'[Fruit Juice] tastes better than [water].',
-		icon:[0,1,'farmsSheet'],
+		icon:[0,1,'imageSheet'],
 		turnToByContext:{'eat':{'health':0.06,'happiness':0.1},'decay':{'spoiled food':0.2}},//this basically translates to : "when eaten, generate some health and happiness; when rotting, turn into either nothing or some spoiled food"
 		partOf:'food',
 		category:'food',
