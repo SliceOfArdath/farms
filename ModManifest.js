cf5d224e7c4c61dc6f91c88149ae60cf6082e6dd
OscarHavril <40829608+OscarHavril@users.noreply.github.com>
2019-05-08 17:03:59 +0200

    Update and rename farmsManifest.js to ModManifest.js

--- ModManifest.js
+++ ModManifest.js
@@ -1,8 +1,9 @@
 G.DeclareManifest({
-name:'farms mod Manifest',
+name:'ModManifest',
 updates:{
   	'Farms*':'https://rawgit.com/OscarHavril/farms/master/[FE]farms.js', //Broken
   //Hmmm it seems to be not working... 
     'FarmsV2*':'https://rawgit.com/OscarHavril/farms/master/[FE]farmsV2.js',
+    'Legacy COOL! Mod':'https://rawgit.com/OscarHavril/farms/master/farming.js',
 }
 });
